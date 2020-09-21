611bb2da4381fb2edd31390fc03408bba575cac1
GMorse19 <malorse@comcast.net>
2020-09-20 21:32:35 -0400

    Three backgrounds
    Temporary

--- src/App.js
+++ src/App.js
@@ -5,27 +5,43 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import './App.scss';
 
 import Header from './Header/Header'
-import Footer from './Footer/Footer'
-// import ShopPage from './ShopPage/ShopPage'
 import HomePage from './HomePage/HomePage'
-// import ContactPage from './ContactPage/ContactPage'
 
-function App() {
+class App extends React.Component{
+  state = {
+    color: 'img1'
+  }
+
+  listenScrollEvent = e => {
+    if (window.scrollY > 700 && window.scrollY <= 2200) {
+      this.setState({color: 'img2 App'})
+    } else if (window.scrollY > 2200) {
+      this.setState({color: 'img3 App'})
+    } else {
+      this.setState({color: 'img1 App'})
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent)
+  }
+
+  render() {
   return (
     <BrowserRouter>
-    <ParallaxProvider>
-      <div className="App">
-        <Header />
-          <div>
-              <Route exact path='/' render={() => (
-                <HomePage />
-              )} />
-          </div>
-        <Footer />
-      </div>
+      <ParallaxProvider>
+        <div className={this.state.color}>
+          <Header />
+            <div>
+                <Route exact path='/' render={() => (
+                  <HomePage />
+                )} />
+            </div>
+        </div>
       </ParallaxProvider>
     </BrowserRouter>
   )
+}
 }
 
 export default App;
