bdfd5c99e9e8854f2424c72539ff39d981c39c73
GMorse19 <malorse@comcast.net>
2020-09-25 16:46:23 -0400

    Added npm package for Horizontal Scroll

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+import HorizontalScroller from 'react-horizontal-scroll-container'
 
 import './Gallery.scss'
 
-const Gallery = () => (
-  <div className='gallery'>
-    <h1>Gallery</h1>
-  </div>
-)
+class Gallery extends Component {
+  render() {
+    return (
+      <div>
+        <HorizontalScroller sensibility={10}>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+          <div className="element"><img className='gallery-image' src='./Flags/B&W/B&W-angle.jpg' alt='black and white'/></div>
+        </HorizontalScroller>
+      </div>
+    );
+  }
+}
 
 export default Gallery
