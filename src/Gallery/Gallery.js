4f5be8726e55aff2f91a134726a03cc4f8c292ae
GMorse19 <malorse@comcast.net>
2020-09-27 22:56:17 -0400

    Added parallax to gallery component
    Happy mistake?
    Tried a couple of npm scrollers, but ended up using parallax
    instead

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Parallax } from 'react-scroll-parallax'
 
 import './Gallery.scss'
 
 const Gallery = () => (
   <div className='gallery'>
-    <h1>Gallery</h1>
+  <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1>NoGallery</h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
+      <h1>Gallery</h1>
+    </Parallax>
   </div>
 )
 
