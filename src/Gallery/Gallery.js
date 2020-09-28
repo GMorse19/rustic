5e70a262704fc02ed4b2ca840f693569b5b90f5e
GMorse19 <malorse@comcast.net>
2020-09-27 23:12:08 -0400

    Added images

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -7,35 +7,35 @@ const Gallery = () => (
   <div className='gallery'>
   <h1></h1>
     <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
-      <h1>Gallery</h1>
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1></h1>
     <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
-      <h1>Gallery</h1>
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1>NoGallery</h1>
     <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
-      <h1>Gallery</h1>
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1></h1>
-    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
-      <h1>Gallery</h1>
-    </Parallax>
-    <h1></h1>
-    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
-      <h1>Gallery</h1>
+    <Parallax scrollAxis='' className="custom-class padding" y={[-90, 90]} tagOuter="figure">
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1></h1>
     <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
-      <h1>Gallery</h1>
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
+    </Parallax>
+    <h1></h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[50, -50]} tagOuter="figure">
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1></h1>
     <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
-      <h1>Gallery</h1>
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
     <h1></h1>
-    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, 50]} tagOuter="figure">
-      <h1>Gallery</h1>
+    <Parallax scrollAxis='horizontal' className="custom-class padding" x={[-50, -50]} tagOuter="figure">
+      <img className='flag-image' src='./Flags/RedWhite&Blue/FlagCutOut.png' alt='Stars and Stripes' />
     </Parallax>
   </div>
 )
