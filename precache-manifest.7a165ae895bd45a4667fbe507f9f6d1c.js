9f9ef942ee6b6228fcfd7afdb4afaa968d217ba2
GMorse19 <malorse@comcast.net>
2020-09-30 16:23:12 -0400

    Updates

---
+++ precache-manifest.7a165ae895bd45a4667fbe507f9f6d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbbe38af64871042391f01047cebced",
+    "url": "/rustic/index.html"
+  },
+  {
+    "revision": "32d3e02298ea34e53cc0",
+    "url": "/rustic/static/css/main.8aedc3cc.chunk.css"
+  },
+  {
+    "revision": "6eacbfbe6dccf145ef5d",
+    "url": "/rustic/static/js/2.47eb48c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/rustic/static/js/2.47eb48c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d3e02298ea34e53cc0",
+    "url": "/rustic/static/js/main.7829e050.chunk.js"
+  },
+  {
+    "revision": "edf0ce805b6c6dd581e0",
+    "url": "/rustic/static/js/runtime-main.6b97857b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f01b88794e44c4dc45b907810b3424bf.js"
+  "/rustic/precache-manifest.7a165ae895bd45a4667fbe507f9f6d1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rustic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
