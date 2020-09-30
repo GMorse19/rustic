4eec461e3a7f03be1f106d8268f0368960d6cfc9
GMorse19 <malorse@comcast.net>
2020-09-30 11:22:10 -0400

    Updates

---
+++ precache-manifest.cc66a3f8e9659cdeb13293e290327f19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f0cf7b0244ef8554021e1cb50874b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9170172fd2180eb33b0",
+    "url": "/static/css/main.701da16b.chunk.css"
+  },
+  {
+    "revision": "1740359648ccecee825a",
+    "url": "/static/js/2.5ab1d1f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5ab1d1f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9170172fd2180eb33b0",
+    "url": "/static/js/main.9a1e79e9.chunk.js"
+  },
+  {
+    "revision": "a4339e1229cfd13e1433",
+    "url": "/static/js/runtime-main.c255495d.js"
+  }
+]);
