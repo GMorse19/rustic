4e4a89a3bb8a79c419208cb00baf1f303ffff0eb
GMorse19 <malorse@comcast.net>
2020-09-30 16:40:10 -0400

    Changed BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+
+const appJsx = (
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+)
+
+ReactDOM.render(appJsx, document.getElementById('root'))
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
